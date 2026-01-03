f3ef2d1785cf7f2944868ba933f6c61c2a997154
Naresh Saini <yogeshsainins100@gmail.com>
2026-01-03 22:44:10 +0530

    setup backend and add db connect mongo route model and controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,38 @@
+import { User } from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+
+export const registerUser = async (req, res)=>{
+    try {
+        const { username, email, password} = req.body;
+        if(!username || !email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+        const existingUser = await User.findOne({email})
+        if(existingUser){
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            })
+        }
+        // hash the password brfore storing in database
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        })
+        return res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            data: newUser
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import "dotenv/config"
+import connectDB from "./database/db.js";
+import userRoute from "./routes/userRoute.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.use('/user', userRoute)
+
+// http://localhost:8000/user/register
+
+app.listen(PORT, ()=>{
+    connectDB();
+    console.log(`Hii yogesh Server is listening at port ${PORT}`);
+    
+})
