05d69a052ed3d8090f0d76437b13c8fde6521c84
Naresh Saini <yogeshsainins100@gmail.com>
2026-01-08 16:37:14 +0530

    add forgetPassword controller  bug fix isauthenticated.js and userRouter.js

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -1,30 +1,30 @@
-import jwt from "jsonwebtoken"
-import { User } from "../models/userModel.js";
+import jwt from 'jsonwebtoken'
+import { User } from '../models/userModel.js';
 
-export const isAuthenticated = async (req, res, next){
+export const isAuthenticated = async(req, res, next) =>{
     try {
         const authHeader = req.headers.authorization;
 
-        if(!authHeader || !authHeader.startsWith("Bearer ")){
+        if(!authHeader || !authHeader.startsWith('Bearer ')){
             return res.status(401).json({
-                success: false,
-                message: "Access token is missing or invalid"
+                success:false,
+                message:'Access token is missing or invalid'
             })
         }
+
         const token = authHeader.split(" ")[1]
 
-        JsonWebTokenError.verify(token, process.env.SECRET_KEY, async (err, decoded)=>{
+        jwt.verify(token, process.env.SECRET_KEY, async (err, decoded)=>{
             if(err){
                 if(err.name === "TokenExpiredError"){
                     return res.status(400).json({
-                        success: false,
-                        message: "Access Token has expired, use refreshtoken to generate again"
+                        success:false,
+                        message:"Access Token has expired, use refreshtoken to generate again"
                     })
-
                 }
                 return res.status(400).json({
-                    success: false,
-                    message: "Access token is missing or invalid"
+                    success:false,
+                    message:"Access token is missing or invalid"
                 })
             }
             const {id} = decoded;
@@ -32,19 +32,19 @@ export const isAuthenticated = async (req, res, next){
             const user = await User.findById(id)
             if(!user){
                 return res.status(404).json({
-                    success: false,
-                    message: "user not found"
+                    success:false,
+                    message:"user not found"
                 })
             }
-
+            
             req.user = user
-            req.userId = user._id
-            next();
+            req.userId = user._id  
+            next()
         })
     } catch (error) {
         return res.status(500).json({
-            succes: false,
-            message: error.message
+            success:false,
+            message:error.message
         })
     }
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { registerUser, verification, loginUser } from "../controllers/userController.js";
+import { registerUser, verification, loginUser, logoutUser } from "../controllers/userController.js";
+import { isAuthenticated } from "../middleware/isAuthenticated.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser)
 router.post("/verify", verification)
 router.post("/login", loginUser)
-router.post("/logout", logoutUser)
+router.post("/logout",isAuthenticated, logoutUser) // path or controller ke bich me aata h voh h middleware 
 export default router;
