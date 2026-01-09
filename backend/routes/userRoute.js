6419fbd46e4ccc47c3e6a83252d4a48c8460f1ee
Naresh Saini <yogeshsainins100@gmail.com>
2026-01-09 14:41:55 +0530

    add verifyOTP, forgotPassword, generateOTP controller and bug fix

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { registerUser, verification, loginUser, logoutUser } from "../controllers/userController.js";
+import { registerUser, verification, loginUser, logoutUser, forgotPassword, verifyOTP } from "../controllers/userController.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.js";
 
 const router = express.Router();
@@ -8,4 +8,7 @@ router.post("/register", registerUser)
 router.post("/verify", verification)
 router.post("/login", loginUser)
 router.post("/logout",isAuthenticated, logoutUser) // path or controller ke bich me aata h voh h middleware 
+router.post("/forgot-password", forgotPassword)
+router.post("/verify-otp/:email", verifyOTP)
+
 export default router;
