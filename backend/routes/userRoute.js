3982f238b6ad95acbc3b5100da3eeeeaad8bc4c2
Naresh Saini <yogeshsainins100@gmail.com>
2026-01-12 15:18:21 +0530

    add userVerify controller bug fixing and setup frontend

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,14 +1,16 @@
 import express from "express"
-import { registerUser, verification, loginUser, logoutUser, forgotPassword, verifyOTP } from "../controllers/userController.js";
+import { registerUser, verification, loginUser, logoutUser, forgotPassword, verifyOTP, changePassword } from "../controllers/userController.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.js";
+import { userSchema, validateUser } from "../validators/userValidate.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser)
+router.post("/register",validateUser(userSchema), registerUser)
 router.post("/verify", verification)
 router.post("/login", loginUser)
 router.post("/logout",isAuthenticated, logoutUser) // path or controller ke bich me aata h voh h middleware 
 router.post("/forgot-password", forgotPassword)
 router.post("/verify-otp/:email", verifyOTP)
+router.post("/change-password/:email", changePassword)
 
 export default router;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-400'>Yogesh Saini</div>
+  )
+}
+
+export default App
