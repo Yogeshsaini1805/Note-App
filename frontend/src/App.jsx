681ca40885eb075e92354306a3109b638b39b104
Naresh Saini <yogeshsainins100@gmail.com>
2026-01-12 17:36:17 +0530

    create login, signupverify pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import {createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import Verify from './pages/Verify.jsx'
+import VerifyEmail from "./pages/VerifyEmail.jsx"
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+  {
+    path: '/verify',
+    element: <VerifyEmail />
+  },
+  {
+    path: '/verify/:token',
+    element: <Verify />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+])
 
 const App = () => {
   return (
-    <div className='text-red-400'>Yogesh Saini</div>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
