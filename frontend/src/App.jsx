12a9b707f93c45be20848f9af83df6013b5398ae
Naresh Saini <yogeshsainins100@gmail.com>
2026-02-18 16:24:36 +0530

    add verify-otp, changePassword jsx file and bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,10 @@ import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
 import Verify from './pages/Verify.jsx'
 import VerifyEmail from "./pages/VerifyEmail.jsx"
+import ForgotPassword from './pages/ForgotPassword.jsx'
+import VerifyOTP from './pages/VerifyOTP.jsx'
 import Navbar from './components/Navbar.jsx'
+import ChangePassword from './pages/ChangePassword.jsx'
 
 const router = createBrowserRouter([
   {
@@ -27,6 +30,18 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login /> 
+  },
+  {
+    path:'/forgot-password',
+    element:<ForgotPassword/>
+  },
+  {
+    path:'/verify-otp/:email',
+    element:<VerifyOTP/>
+  },
+  {
+    path:'/change-password/:email',
+    element:<ChangePassword/>
   }
 ])
 
